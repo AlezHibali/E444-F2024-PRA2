3ede842abeb5c01013b2b9fcdfd5fcecd7997d78
AlezHibali <ali.daixin.tian@gmail.com>
2024-09-23 00:35:51 -0400

    add dynamic route /user/<name> to display different response based on given name

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+# flask --app hello run
+# correct command to run flask app
+# refer: https://flask.palletsprojects.com/en/2.3.x/cli/#run-the-development-server
 @app.route('/') 
 def index(): 
     return '<h1>Hello World!</h1>'
 
-# flask --app hello run
-# correct command to run flask app
-# refer: https://flask.palletsprojects.com/en/2.3.x/cli/#run-the-development-server
+@app.route('/user/<name>')
+def user(name):
+    return'<h1>Hello, {}!</h1>'.format(name)
