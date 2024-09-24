ed1f6f9383237918a58135fd65db63f1eb25015b
AlezHibali <ali.daixin.tian@gmail.com>
2024-09-24 15:11:57 -0400

    add email input in session and its flash message

--- README.md
+++ README.md
@@ -3,7 +3,15 @@ Flasky
 
 Daixin Tian
 This repo is a clone of https://github.com/miguelgrinberg/flasky.
-======
 
 ## Activity 1.1
 ![Alt text](assets/img/activity11.png)
+
+## Activity 1.2
+Overwritten by Activity 1.3
+
+## Activity 1.3
+![Alt text](assets/img/activity13.png)
+
+## Activity 1.4
+![Alt text](assets/img/activity13.png)
--- hello.py
+++ hello.py
@@ -20,24 +20,32 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class NameForm(FlaskForm):
+class NameEmailForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email Address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 '''
 App Function Definitions
 '''
 # view function: handle a web form with GET and POST request methods
+# Prevents Duplicate Form Submissions
 @app.route('/',methods=['GET','POST'])
 def index():
-    form = NameForm()
+    form = NameEmailForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
+        old_name, old_email = session.get('name'), session.get('email')
+        # check for flash message
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+            
         session['name']= form.name.data
+        session['email']= form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'), 
+                           email=session.get('email'), current_time=datetime.utcnow())
 
 # make <name> optional
 @app.route('/user/')
